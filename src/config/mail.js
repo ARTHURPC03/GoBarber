47d77dc4ead6accd3b00b7dc1f682af1648c3a01
ARTHUR PC <arthurpimetelcordeiro@gmail.com>
2020-02-27 14:55:21 -0300

    configurando nodemailer

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: 'f6264732353027',
+    pass: '950f7b2cbd3e78',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+}
